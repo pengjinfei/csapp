bd73d66969f2728c23853c08ca1223fa96f81875
pengjinfei <pengjinfei@126.com>
2018-05-02 21:15:10 +0800

    Initial commit

---
+++ lib/lib.h
@@ -0,0 +1,10 @@
+//
+// Created by 彭金飞 on 5/2/18.
+//
+
+#ifndef CSAPP_LIB_H
+#define CSAPP_LIB_H
+
+extern void test_show_bytes(int x);
+
+#endif //CSAPP_LIB_H
---
+++ main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "lib/lib.h"
+
+int main() {
+    test_show_bytes(12345);
+    return 0;
+}
