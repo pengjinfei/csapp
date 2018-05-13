18a5b88b9947e7772e669b783e438bf34a606e5e
pengjinfei <pengjinfei@126.com>
2018-05-13 15:55:48 +0800

    gitlab

--- lib/lib.h
+++ lib/lib.h
@@ -6,13 +6,19 @@
 #define CSAPP_LIB_H
 
 extern void show_int(int x);
+
 extern void show_float(float x);
+
 extern void show_pointer(void *x);
+
 extern void show_chars(char *s);
+
 extern void show_short(short x);
+
 extern void test_show_bytes(int x);
 
-extern int bool_or(int x,int m);
-extern int bool_xor(int x,int y);
+extern int bool_or(int x, int m);
+
+extern int bool_xor(int x, int y);
 
 #endif
--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <inttypes.h>
 #include "lib/lib.h"
+#include "lib/bits.h"
 
 int equals(int x,int y){
     return !(x ^ y);
@@ -27,6 +28,22 @@ int main() {
     show_short(ma);
     show_short(ua);
 
+    printf(" %d bitAnd %d = %d\n", 1, 2, bitAnd(1, 2));
+    printf(" %d bitAnd %d = %d\n", 1, 3, bitAnd(1, 3));
+    printf(" %d bitAnd %d = %d\n", 1, 4, bitAnd(1, 4));
+
+    printf("getByte(0x%x,%d) = 0x%x\n", 0x12345678,1,getByte(0x12345678,0));
+    printf("getByte(0x%x,%d) = 0x%x\n", 0x12345678,1,getByte(0x12345678,1));
+    printf("getByte(0x%x,%d) = 0x%x\n", 0x12345678,1,getByte(0x12345678,2));
+    printf("getByte(0x%x,%d) = 0x%x\n", 0x12345678,1,getByte(0x12345678,3));
+
+    printf("logicalShift(0x%08x,%d) = 0x%08x\n", 0x87654321,0,logicalShift(0x87654321,0));
+    printf("logicalShift(0x%08x,%d) = 0x%08x\n", 0x87654321,1,logicalShift(0x87654321,1));
+    printf("logicalShift(0x%08x,%d) = 0x%08x\n", 0x87654321,2,logicalShift(0x87654321,2));
+    printf("logicalShift(0x%08x,%d) = 0x%08x\n", 0x87654321,3,logicalShift(0x87654321,3));
+    printf("logicalShift(0x%08x,%d) = 0x%08x\n", 0x87654321,4,logicalShift(0x87654321,4));
+    printf("logicalShift(0x%08x,%d) = 0x%08x\n", 0x87654321,8,logicalShift(0x87654321,8));
+    printf("logicalShift(0x%08x,%d) = 0x%08x\n", 0x87654321,8,logicalShift(0x87654321,31));
     return 0;
 }
 
