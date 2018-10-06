9aabe5fa962acba5107c3c9c088929cee9a3251c
pengjinfei <pengjinfei@126.com>
2018-10-07 00:07:24 +0800

    add cachelab pdf

--- labs/cache/csim.c
+++ labs/cache/csim.c
@@ -1,7 +1,16 @@
 #include "cachelab.h"
+#include <getopt.h>
+#include <unistd.h>
 
-int main()
+int main(int argc, char * argv[])
 {
+    int ch;
+    while((ch=getopt(argc,argv,"hvs:E:b:t:"))!=-1) {
+        switch (ch) {
+            case 'h':
+                break;
+        }
+    }
     printSummary(0, 0, 0);
     return 0;
 }
